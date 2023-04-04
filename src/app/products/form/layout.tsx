1e279c19412961de1d305ad84eef22f4804a90f7
Octavio Martinez <omartinez1618@gmail.com>
2023-04-04 13:19:56 -0400

    feat(products, recipes): add form layout components
    Add form layout components for products and recipes. These components
    will be used to wrap the form pages and provide a consistent layout for
    the forms.

---
+++ src/app/products/form/layout.tsx
@@ -0,0 +1,8 @@
+export default function ProductFormLayout ({ children }:{children:any}) {
+
+    return <div>
+        <p>este es el formularion para los productos</p>
+        { children }
+    </div>
+
+}
---
+++ src/app/recipes/form/layout.tsx
@@ -0,0 +1,7 @@
+export default function RecipeFormLayout ({ children }:{ children:any }) {
+
+    return <div>
+        <p>este es el formularion para los recetas</p>
+        { children }
+    </div>
+}
