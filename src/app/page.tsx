b758d4265ea868e1b18f7888f4dc2ad842685c0d
Octavio Martinez <omartinez1618@gmail.com>
2023-04-05 09:22:16 -0400

    style(global.css): add styles to cards and main container feat(page.tsx): add links to products and recipes pages feat(recipes/page.tsx): add ListParent component to wrap recipe links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-
+import Link from "next/link"
 
 export default function Home() {
-  return (<p>Recipe-food</p>)
+  return (<div className="main">
+    <Link className="card" href='/products'>Productos</Link>
+    <Link className="card" href='/recipes'>Recetas</Link>
+  </div>)
 }
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import ListParent from "../components/ListParent";
 
 export default function RecipeListPage () {
 
     return(
-        <ul>
+        <ListParent>
             <li><Link href={`/recipes/1`}>Receta 1</Link></li>
             <li><Link href={`/recipes/2`}>Receta 2</Link></li>
             <li><Link href={`/recipes/3`}>Receta 3</Link></li>
             <li><Link href={`/recipes/4`}>Receta 4</Link></li>
-        </ul>
+        </ListParent>
     )
 }
