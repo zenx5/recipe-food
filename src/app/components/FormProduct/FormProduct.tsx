ae1027a7a7b02d405496c6239af3ba90537cf5f4
Octavio Martinez <omartinez1618@gmail.com>
2023-04-08 12:40:00 -0400

    style(FormProduct): add CSS module to FormProduct component and refactor styles to use it feat(FormProduct): add quantity input to FormProduct component feat(FormProduct): add submit handler to FormProduct component to send product data to server

--- src/app/components/FormProduct/FormProduct.tsx
+++ src/app/components/FormProduct/FormProduct.tsx
@@ -3,39 +3,81 @@
 import { productType } from "@/app/types";
 import { useState } from "react";
 
+import styles from './FormProduct.module.css'
+
 export default function FormProduct ({ product }:{ product:productType }) {
     console.log(product)
     const {
         id=0,
-        name:nameDefault='',
-        price:priceDefault=0,
-        active:activeDefault=false
+        name:nameDefault = '',
+        price:priceDefault = 0,
+        active:activeDefault = false,
+        quantity:quantityDefault = 0
     } = product
 
     const [name, setName] = useState(nameDefault)
     const [price, setPrice] = useState(priceDefault)
     const [active, setActive] = useState(activeDefault)
+    const [quantity, setQuantity] = useState(quantityDefault)
 
 
     const handlerChangeName = ({ target: { value:newName } }:{ target: { value:string } } ) => {
         setName( () => newName )
     }
 
-    const handlerChangePrice = ({ target: { value:newPrice } }:{ target: { value:number } } ) => {
-        setPrice( () => newPrice )
+    const handlerChangeQuantity = ({ target: { value:newQuantity } }:{ target: { value:string } } ) => {
+        setQuantity( () => newQuantity )
     }
 
-    return <form>
-        <div>
-            <input type='text' placeholder="name" value={name} onChange={handlerChangeName} />
-            <input type='number' placeholder="price" value={price} onChange={handlerChangePrice} />
+    const handlerChangePrice = ({ target: { value:newPrice } }:{ target: { value:string } } ) => {
+        setPrice( () => parseFloat(newPrice) )
+    }
+
+    const handlerSubmit = async (eventSubmit:any) => {
+        eventSubmit.preventDefault()
+        const response = await fetch('/api/products', {
+            method:'post',
+            body: JSON.stringify({
+                name,
+                price,
+                active,
+                quantity
+            })
+        })
+        console.log( response )
+    }
+
+    return <form className={styles.form} onSubmit={handlerSubmit}>
+        <div className={styles.row}>
+            <input
+                className={styles.input}
+                type='text'
+                placeholder='name'
+                value={name}
+                onChange={handlerChangeName} />
+            <input
+                className={styles.input}
+                type='number'
+                placeholder='price'
+                value={price}
+                onChange={handlerChangePrice} />
         </div>
-        <div>
-            <label>
-                <input type='checkbox' />
-                Active
+        <div className={styles.row}>
+            <input
+                className={styles.input}
+                type='text'
+                placeholder='quantity'
+                value={quantity}
+                onChange={handlerChangeQuantity} />
+            <label className={styles.checkbox}>
+                <div className={active ? styles.active : styles.deactive}>
+                    <input type='checkbox' onClick={()=>setActive(!active)}/>
+                </div>
+                {active ? 'Active' : 'Deactive'}
             </label>
-            <button>Save</button>
+        </div>
+        <div className={styles.row}>
+            <button className="button">Save</button>
         </div>
     </form>
 }
