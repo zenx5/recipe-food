ade874cf08c16ed02c4e9620681cbd30f774d03f
Octavio Martinez <omartinez1618@gmail.com>
2023-04-08 12:34:58 -0400

    feat(layout.tsx): add a div container with class 'main-container' to wrap children components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="main-container">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
