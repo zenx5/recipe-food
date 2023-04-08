79fc892875704fa7e394875ad693235048e03a04
Octavio Martinez <omartinez1618@gmail.com>
2023-04-08 12:40:53 -0400

    feat(product-form): add default product object to ProductFormLayout component

--- src/app/products/form/layout.tsx
+++ src/app/products/form/layout.tsx
@@ -1,10 +1,18 @@
 import { FormProduct } from "@/app/components";
+import { productType } from "@/app/types";
 
 export default function ProductFormLayout ({ children }:{children:any}) {
 
+    const product:productType = {
+        id: 0,
+        name: '',
+        price: 0,
+        active: false,
+        quantity: 0
+    };
 
     return <div>
-        { children ? <FormProduct product={{}}/> : children }
+        { children ? <FormProduct product={product}/> : children }
     </div>
 
 }
