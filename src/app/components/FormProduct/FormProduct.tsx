c43dbee7aa427bc12f1c42465598e5d5f0eab669
Octavio Martinez <omartinez1618@gmail.com>
2023-04-10 01:21:22 -0400

    feat(CheckItem.tsx): add FontAwesomeIcon component to replace button element feat(FormProduct.tsx): add validation for required fields and disable submit button if required fields are empty

--- src/app/components/FormProduct/FormProduct.tsx
+++ src/app/components/FormProduct/FormProduct.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { productType } from "@/app/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from './FormProduct.module.css'
 
@@ -15,18 +15,39 @@ export default function FormProduct ({ product }:{ product:productType }) {
         quantity:quantityDefault = ''
     } = product
 
+    const [required, setRequired] = useState(null)
     const [name, setName] = useState(nameDefault)
     const [price, setPrice] = useState(priceDefault)
     const [active, setActive] = useState(activeDefault)
     const [quantity, setQuantity] = useState(quantityDefault)
 
+    useEffect( () => {
+        if ( required === null ){
+            validateRequired('name',name)
+            validateRequired('quantity',quantity)
+        }
+    }, [required])
+
+
+    const validateRequired = (field:string, currentValue:string) => {
+        if( currentValue!=='' ) {
+            setRequired( (prev:any) => (prev || []).filter( (item:string) => item !== field ) )
+        } else {
+            setRequired( (prev:any) => {
+                if( (prev || []).includes(field) ) return (prev || [])
+                return [ field, ...(prev || []) ]
+            } )
+        }
+    }
 
     const handlerChangeName = ({ target: { value:newName } }:{ target: { value:string } } ) => {
         setName( () => newName )
+        validateRequired('name', newName)
     }
 
     const handlerChangeQuantity = ({ target: { value:newQuantity } }:{ target: { value:string } } ) => {
         setQuantity( () => newQuantity )
+        validateRequired('quantity', newQuantity)
     }
 
     const handlerChangePrice = ({ target: { value:newPrice } }:{ target: { value:string } } ) => {
@@ -35,16 +56,21 @@ export default function FormProduct ({ product }:{ product:productType }) {
 
     const handlerSubmit = async (eventSubmit:any) => {
         eventSubmit.preventDefault()
-        const response = await fetch('/api/products', {
-            method:'post',
-            body: JSON.stringify({
-                name,
-                price,
-                active,
-                quantity
+        if ( required && required.length>0 ) return;
+        try {
+            const response = await fetch('/api/products', {
+                method:'post',
+                body: JSON.stringify({
+                    name,
+                    price,
+                    active,
+                    quantity
+                })
             })
-        })
-        console.log( response )
+            document.location.pathname = id ? `/products/${id}` : '/products'
+        } catch ( error ) {
+            console.log( error )
+        }
     }
 
     return <form className={styles.form} onSubmit={handlerSubmit}>
@@ -54,6 +80,7 @@ export default function FormProduct ({ product }:{ product:productType }) {
                 type='text'
                 placeholder='name'
                 value={name}
+                style={ required ? (required.includes('name') ? { border:'1px solid red' } : {}) : {} }
                 onChange={handlerChangeName} />
             <input
                 className={styles.input}
@@ -67,6 +94,7 @@ export default function FormProduct ({ product }:{ product:productType }) {
                 className={styles.input}
                 type='text'
                 placeholder='quantity'
+                style={ required ? (required.includes('quantity') ? { border:'1px solid red' } : {}) : {} }
                 value={quantity}
                 onChange={handlerChangeQuantity} />
             <label className={styles.checkbox}>
@@ -77,7 +105,7 @@ export default function FormProduct ({ product }:{ product:productType }) {
             </label>
         </div>
         <div className={styles.row}>
-            <button className="button">Save</button>
+            <button className="button" disabled>Save</button>
             <Link className="button" href={ id ? `/products/${id}` : '/products' }>Back</Link>
         </div>
     </form>
